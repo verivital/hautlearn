9048f018205b269b9ebd907a1f7ae2b21ab086a0
Shaddadi <32499911+Shaddadi@users.noreply.github.com>
2019-03-27 16:55:39 -0500

    Add plot function for buck-converter

--- example/buck_converter/trainingdata/generateTraces.m
+++ example/buck_converter/trainingdata/generateTraces.m
@@ -4,8 +4,8 @@ mdl1 = '.\buck_dcm_vs1';
 mdl2 = '.\automata_learning';
 T = 0.02; % running time
 Ts = 0.00005; % sample time
-% il_init = rand*(-20)-10; % range(0,15)
-% vc_init = 0; %rand*20+10; % range(0,30)
+% x0(1) = rand*(-20)-10; % range(0,15)
+% x0(2) = 0; %rand*20+10; % range(0,30)
 sim(mdl2)
 
 plot(xout)
